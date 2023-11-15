42e41d3c41cb0152c12830c60ae50238df0253cd
jonwilm <jparedes84@gmail.com>
2023-11-15 17:12:06 -0400

    pos.csv

--- scripts/import_pos.py
+++ scripts/import_pos.py
@@ -2,11 +2,13 @@ import csv
 from apps.generales.models import PointOfSale
 
 def run():
-    with open('/home/jonwilm/Documentos/Development/proyectos/isakito/Material/pos.csv', 'r') as f:
+    with open('/home/jonwilm/Documentos/Development/proyectos/isakito/Material/PointOfSale-2023-11-13.csv', 'r') as f:
         reader = csv.reader(f, delimiter=',')
         for row in reader:
             pos = PointOfSale()
             pos.name = row[0]
             pos.address = row[1]
             pos.locality = row[2]
+            pos.lat = row[3]
+            pos.lng = row[4]
             pos.save()
