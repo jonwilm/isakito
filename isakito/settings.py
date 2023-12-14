50066eaae3ceae797fc3974dfffe5bfcfc1b173c
jonwilm <jparedes84@gmail.com>
2023-12-14 16:25:34 -0400

    debug-false

--- isakito/settings.py
+++ isakito/settings.py
@@ -1,7 +1,7 @@
 from .base_settings import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Production
 # -------------------------------------
